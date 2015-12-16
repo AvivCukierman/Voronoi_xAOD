ca29d056b465f92ea9160fb0b078dcc5eb62bf70
AvivCukierman <acukierm@slac.stanford.edu>
2015-12-16 10:57:13 -0800

    Update run_lsf_test.sh

--- scripts/run_lsf_test.sh
+++ scripts/run_lsf_test.sh
@@ -3,7 +3,7 @@ python MyAnalysis/scripts/Run.py \
     --submitDir "inputDQ2_test" \
     --inputDQ2 \
     --inputFiles "mc15_13TeV.361021.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ1W.merge.AOD.e3569_s2576_s2132_r6765_r6282/" \
-    --driver "direct" \
+    --driver "lsf" \
     -w \
     --nevents 10
 exit 0
