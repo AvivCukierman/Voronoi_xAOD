8e321de9cdf295502f28368f0ab4ce085e4610ba
Aviv Cukierman <acukierm@slac.stanford.edu>
2016-06-13 11:35:18 -0700

    Removed run scripts that don't use VoronoiWeightTool because it was confusing.

--- scripts/run_lsf_test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python MyAnalysis/scripts/Run.py \
-    --submitDir "inputDQ2_test" \
-    --inputDQ2 \
-    --inputFiles "mc15_13TeV.361021.Pythia8EvtGen_A14NNPDF23LO_jetjet_JZ1W.merge.AOD.e3569_s2576_s2132_r6765_r6282/" \
-    --driver "lsf" \
-    -w \
-    --nevents 10
-exit 0
